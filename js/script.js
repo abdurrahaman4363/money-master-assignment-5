156b766e05eb4a2ab7a7b86f27c7129f035afbcc
abdurrahaman4363 <abdur.rahaman4363@gmail.com>
2022-02-17 11:57:47 +0600

    total expense calculate

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
 document.getElementById('calculate').addEventListener('click', function(){
+    const foodInput = document.getElementById('food-input');
+    const foodValue = foodInput.value;
+    foodInput.value= "";
+    
+
+    const rentInput = document.getElementById('rent-input');
+    const rentValue = rentInput.value;
+    rentInput.value= "";
+    
+    
+
+    const clothesInput = document.getElementById('clothes-input');
+    const clothesValue = clothesInput.value;
+    clothesInput.value= "";
+    
+    const totalExpenses = parseFloat(foodValue) + parseFloat(rentValue) + parseFloat(clothesValue);
     
 })
